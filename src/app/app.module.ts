7a9ccf5591d4b2411555791af7232d301be45d92
SerenityDevelopmentGroup <fordff@serenitydevelopmentgroup.com>
2017-02-05 23:05:53 -0800

    ArtWorkNG

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { PackageComponent } from './package/package.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ArtworksComponent } from './artworks/artworks.component';
+import { AdminComponent } from './admin/admin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PackageComponent,
+    ProjectsComponent,
+    ArtworksComponent,
+    AdminComponent
   ],
   imports: [
     BrowserModule,
